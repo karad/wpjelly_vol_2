cc44ea28499d79ec6d34894ff85946d62003300b
kazuhiro hara <karad@fxb.jp>
2011-10-20 18:54:14 +0900

    update php file

--- wpjelly_admin_widget/wpjelly_admin_widget.php
+++ wpjelly_admin_widget/wpjelly_admin_widget.php
@@ -37,27 +37,16 @@ function wpjelly_admin_widget() {
 <h2>現在の忙しさウィジェット</h2>
 <p>現在の忙しさをアピールしましょう！</p>
 EOF;
-    if ( !empty($_POST['posted']) && $_POST['posted'] === 'yes' ) {
-        check_admin_referer('wpjelly_admin_widget', 'greative');
-		update_option('admin_widget', strip_tags($_POST['admin_widget']));
-        echo '<div class="updated"><p><strong>忙しさを保存しました</strong></p></div>';
-    }
+    // ここに挿入1
 	$str .=  <<< EOF
 <form action="" method="post">
 <select name="admin_widget" id="admin_widget">
 EOF;
-    $works = array(
-        array('value' => '忙しい', 'text' => '忙しい'),
-        array('value' => '普通', 'text' => '普通'),
-        array('value' => 'ひま', 'text' => 'ひま'),
-    );
-    $selected_value = get_option('admin_widget');
-    foreach ($works as $work) {
-        $selected = ($selected_value == $work['value']) ? ' selected="selected"' : '';
-        $str .= '<option value="' . esc_attr($work['value']) . '" ' . $selected . '>' . esc_attr($work['text']) . '</option>';
-    }
+    // ここに挿入2-1
+    // ここに挿入2-2
+    // ここに挿入2-3
     $str .= '</select>';
-    $str .= wp_nonce_field('wpjelly_admin_widget', 'greative', true, false);
+    // ここに挿入3
 	$str .=  <<< EOF
 <input type="hidden" name="posted" value="yes" />
 <p class="submit"><input type="submit" name="Submit" class="button-primary" value="保存" /></p>
@@ -67,24 +56,9 @@ EOF;
 	echo $str;
 }
 
+// ここに挿入4
 
-
-class AdminWidget extends WP_Widget {
-    function AdminWidget() {
-        parent::WP_Widget(false, $name = '現在の忙しさ');
-    }
-    function widget($args, $instance) {
-        extract( $args );
-        echo $before_widget;
-        echo $before_title . $widget_name . $after_title;
-        $admin_widget = get_option('admin_widget');
-        echo '<p>' . $admin_widget . '</p>';
-        echo $after_widget;
-    }
-}
-
-add_action('widgets_init', create_function('', 'return register_widget("AdminWidget");'));
-
+// ここに挿入5
 
 
 ?>
